d27d30a22ceb58af5b7bece02bfc1330da6f5ab4
Anton Moroz <anton.moroz@helsinki.fi>
2017-11-18 21:13:04 +0200

    Tiedostoja tyhjennetty

--- src/main/java/ohtu/Controllers.java
+++ src/main/java/ohtu/Controllers.java
@@ -1,20 +1,11 @@
 package ohtu;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
 public class Controllers {
 
-    @GetMapping("/")
-    @ResponseBody
     public String home() {
-        return "<a href='/other'>linkki</a>";
+        return "";
     }
-    
-    @GetMapping("/other")
-    @ResponseBody
+
     public String other() {
         return "Hei Maailma!";
     }
--- src/test/java/ohtu/Stepdefs.java
+++ src/test/java/ohtu/Stepdefs.java
@@ -16,16 +16,7 @@ public class Stepdefs {
     WebDriver driver;
 
     public Stepdefs() {
-        File file;
-        if (System.getProperty("os.name").matches("Mac OS X")) {
-            file = new File("lib/macgeckodriver");
-        } else {
-            file = new File("lib/geckodriver");
-        }
-        String absolutePath = file.getAbsolutePath();
-        System.setProperty("webdriver.gecko.driver", absolutePath);
 
-        this.driver = new FirefoxDriver();
     }
 
     @After
@@ -33,36 +24,4 @@ public class Stepdefs {
         driver.quit();
     }
 
-    @Given("^user is at the main page$")
-    public void user_is_at_the_main_page() throws Throwable {
-        driver.get("http://localhost:" + 8080 + "/" );
-        Thread.sleep(1000);        
-    }
-
-    @When("^a link is clicked$")
-    public void a_link_is_clicked() throws Throwable {
-        Thread.sleep(1000);  
-        clickLinkWithText("linkki" );
-        Thread.sleep(1000);  
-    }    
-   
-    @Then("^\"([^\"]*)\" is shown$")
-    public void is_shown(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.tagName("body"))
-                .getText().contains(arg1));
-    }
-
-    private void clickLinkWithText(String text) {
-        int trials = 0;
-        while( trials++<5 ) {
-            try{
-                WebElement element = driver.findElement(By.linkText(text));
-                element.click();
-                break;           
-            } catch(Exception e) {
-                System.out.println(e.getStackTrace());
-            }
-        }
-    }
-    
 }
