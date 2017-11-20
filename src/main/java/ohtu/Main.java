c9e7c07a4271e5df7d2e3d980359af32f9a6b210
Moroz Anton <amoroz@da5-cs-b221-27.pc.helsinki.fi>
2017-11-20 13:57:00 +0200

    SQLITE/JDBC-tuki lisätty gradleen, testiyhteys muodostettu tietokantaan

--- src/main/java/ohtu/Main.java
+++ src/main/java/ohtu/Main.java
@@ -1,8 +1,18 @@
 package ohtu;
 
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:tietokanta.db");
+        PreparedStatement statement = connection.prepareStatement("SELECT 1");
+        ResultSet rs = statement.executeQuery();
+        if (rs.next()) {
+            System.out.println("Yhteys toimii");
+        } else {
+            System.out.println("Ongelma...");
+        }
 
     }
 }
