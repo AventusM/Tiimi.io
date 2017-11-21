d8600a89f46a3a3ac089dbeaa8035b51142537d8
Lauri Karanko <lauri.karanko@gmail.com>
2017-11-21 11:47:31 +0200

    tietokanta.db lisätty testidataa, muutettu tietokantoja selkeämmäksi.

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -12,18 +12,18 @@ dateAdded DATE NOT NULL
 CREATE TABLE Courses(
 course_id SERIAL PRIMARY KEY NOT NULL,
 courseName varchar(50)
-}
+);
 
 -- Liitostaulu
 
 CREATE TABLE BookPrequisiteCourses(
 book_id int REFERENCES Book(id) NOT NULL,
-course int REFERENCES Courses(course_id) NOT NULL, 
+course int REFERENCES Courses(course_id) NOT NULL 
 );
 
 -- Liitostaulu
 
 CREATE TABLE BookRelatedCourses(
 book_id int REFERENCES Book(id) NOT NULL,
-course_id int REFERENCES Courses(course_id) NOT NULL, 
+course_id int REFERENCES Courses(course_id) NOT NULL 
 );
